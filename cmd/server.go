34fac98aacd089109fb47c72a335dcb67663c1c5
Noboru Saito <noborusai@gmail.com>
2021-01-05 11:52:08 +0900

    upgrade ov v0.8.0
    Fixed ov options to be used correctly.

--- cmd/server.go
+++ cmd/server.go
@@ -38,7 +38,6 @@ func receive(ov *oviewer.Root, l net.Listener) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("%#v %#v\n", ov.Config.General.AlternateRows, doc.AlternateRows)
 		doc.ReadAll(conn)
 		ov.AddDocument(doc)
 	}
@@ -52,7 +51,8 @@ func server() {
 
 	doc, err := oviewer.NewDocument()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	s := SockAddr
@@ -60,12 +60,15 @@ func server() {
 
 	ov, err := oviewer.NewOviewer(doc)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	ov.SetConfig(config)
 
 	l, err := net.Listen("unix", SockAddr)
 	if err != nil {
+		fmt.Printf("listen error: %s", err.Error())
+		os.Exit(1)
 		log.Fatal("listen error:", err)
 	}
 	defer l.Close()
@@ -73,7 +76,8 @@ func server() {
 	go receive(ov, l)
 
 	if err := ov.Run(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
@@ -90,26 +94,12 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVarP(&SockAddr, "socket", "p", SockAddr, "socket path ")
 
-	serverCmd.PersistentFlags().BoolP("wrap", "w", true, "wrap mode")
-	_ = viper.BindPFlag("general.Wrap", serverCmd.PersistentFlags().Lookup("wrap"))
-
+	// Config.General
 	serverCmd.PersistentFlags().IntP("tab-width", "x", 8, "tab stop width")
 	_ = viper.BindPFlag("general.TabWidth", serverCmd.PersistentFlags().Lookup("tab-width"))
 
 	serverCmd.PersistentFlags().IntP("header", "H", 0, "number of header rows to fix")
 	_ = viper.BindPFlag("general.Header", serverCmd.PersistentFlags().Lookup("header"))
-
-	serverCmd.PersistentFlags().BoolP("disable-mouse", "", false, "disable mouse support")
-	_ = viper.BindPFlag("general.DisableMouse", serverCmd.PersistentFlags().Lookup("disable-mouse"))
-
-	serverCmd.PersistentFlags().BoolP("exit-write", "X", false, "output the current screen when exiting")
-	_ = viper.BindPFlag("general.ExitWrite", serverCmd.PersistentFlags().Lookup("exit-write"))
-
-	serverCmd.PersistentFlags().BoolP("quit-if-one-screen", "F", false, "quit if the output fits on one screen")
-	_ = viper.BindPFlag("general.QuitSmall", serverCmd.PersistentFlags().Lookup("quit-if-one-screen"))
-
-	serverCmd.PersistentFlags().BoolP("case-sensitive", "i", false, "case-sensitive in search")
-	_ = viper.BindPFlag("general.CaseSensitive", serverCmd.PersistentFlags().Lookup("case-sensitive"))
 
 	serverCmd.PersistentFlags().BoolP("alternate-rows", "C", false, "color to alternate rows")
 	_ = viper.BindPFlag("general.AlternateRows", serverCmd.PersistentFlags().Lookup("alternate-rows"))
@@ -117,9 +107,28 @@ func init() {
 	serverCmd.PersistentFlags().BoolP("column-mode", "c", false, "column mode")
 	_ = viper.BindPFlag("general.ColumnMode", serverCmd.PersistentFlags().Lookup("column-mode"))
 
+	serverCmd.PersistentFlags().BoolP("line-number", "n", false, "line number")
+	_ = viper.BindPFlag("general.LineNumMode", serverCmd.PersistentFlags().Lookup("line-number"))
+
+	serverCmd.PersistentFlags().BoolP("wrap", "w", true, "wrap mode")
+	_ = viper.BindPFlag("general.WrapMode", serverCmd.PersistentFlags().Lookup("wrap"))
+
 	serverCmd.PersistentFlags().StringP("column-delimiter", "d", ",", "column delimiter")
 	_ = viper.BindPFlag("general.ColumnDelimiter", serverCmd.PersistentFlags().Lookup("column-delimiter"))
 
-	serverCmd.PersistentFlags().BoolP("line-number", "n", false, "line number")
-	_ = viper.BindPFlag("general.LineNumMode", serverCmd.PersistentFlags().Lookup("line-number"))
+	// Config
+	serverCmd.PersistentFlags().BoolP("disable-mouse", "", false, "disable mouse support")
+	_ = viper.BindPFlag("DisableMouse", serverCmd.PersistentFlags().Lookup("disable-mouse"))
+
+	serverCmd.PersistentFlags().BoolP("exit-write", "X", false, "output the current screen when exiting")
+	_ = viper.BindPFlag("AfterWrite", serverCmd.PersistentFlags().Lookup("exit-write"))
+
+	serverCmd.PersistentFlags().BoolP("quit-if-one-screen", "F", false, "quit if the output fits on one screen")
+	_ = viper.BindPFlag("QuitSmall", serverCmd.PersistentFlags().Lookup("quit-if-one-screen"))
+
+	serverCmd.PersistentFlags().BoolP("case-sensitive", "i", false, "case-sensitive in search")
+	_ = viper.BindPFlag("CaseSensitive", serverCmd.PersistentFlags().Lookup("case-sensitive"))
+
+	serverCmd.PersistentFlags().BoolP("debug", "", false, "debug mode")
+	_ = viper.BindPFlag("Debug", serverCmd.PersistentFlags().Lookup("debug"))
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.15
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/noborus/ov v0.7.1-0.20201231084133-fc526c6bad5c
+	github.com/noborus/ov v0.8.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20210104204734-6f8348627aad // indirect
 )
